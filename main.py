fe3e90ca5c56c6bc4e1188a115c0e554c70b449e
lakshya pachkhede <pachkhedelakshya@gmail.com>
2026-06-19 22:32:36 +0530

    added google verification file

--- main.py
+++ main.py
@@ -256,6 +256,7 @@ def home():
 def about():
     return render_template('about.html')
 
+
 @app.template_filter("branch_name")
 def branch_name(code):
     return BRANCHES.get(code, code)
@@ -270,6 +271,10 @@ def robots():
 @app.route("/sitemap.xml")
 def sitemap():
     return send_from_directory("static", "sitemap.xml")
+
+@app.route('/google3bf79a468e1ed3d9.html', methods=['GET'])
+def about():
+    return send_from_directory("static", "google3bf79a468e1ed3d9.html")
 
 
 if __name__ == '__main__':
